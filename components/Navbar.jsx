a021f088c9819dcf1a0900090b1158afa6132d44
mohmad-sawan <mohmad-sa.99@outlook.com>
2023-01-04 09:51:25 +0300

    create context file, and netlify login

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,12 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import { useContext } from 'react'
+import AuthContext from '../stores/authContext'
 
 export default function Navbar() {
+  const {user, login} = useContext(AuthContext)
+  console.log(user)
+
   return (
     <div className="container">
       <nav>
@@ -10,10 +15,11 @@ export default function Navbar() {
         <ul>
           <li><Link href="/">Home</Link></li>
           <li><Link href="/guides">Guides</Link></li>
+          <li onClick={login} className="btn">Login/sinout</li>
         </ul>
       </nav>
       <div className="banner">
-        <Image src="/banner.png" width={966} height={276} />
+        <Image src="/banner.png" alt='banner nav' width={966} height={276} />
       </div>
     </div>
   )
---
+++ stores/authContext.jsx
@@ -0,0 +1,31 @@
+import { createContext, useEffect, useState } from "react";
+import netlifyIdentity from "netlify-identity-widget"
+
+const AuthContext = createContext({
+    user: null,
+    login: () => {},
+    logout: () => {},
+    authReady: false
+});
+
+export const AuthContextProvider = ({children}) => {
+    const [user, setUser] = useState(null)
+
+    useEffect(()=>{
+        // init netlify identity connection
+        netlifyIdentity.init()
+    },[])
+    const login = ()=> {
+        netlifyIdentity.open()
+    }
+
+    const context = {user, login } 
+
+    return(
+        <AuthContext.Provider value={context}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export default AuthContext;
